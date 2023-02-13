0e9f69807c2e3debccadaf79a2e0944e549a3e6c
lluisg <lluis.guardia.f@gmail.com>
2023-02-13 19:19:48 +0100

    db selector+small fixes
    added databases type projects and some small modifications on media queries

--- js/scripts_new.js
+++ js/scripts_new.js
@@ -8,7 +8,7 @@ function activateFilter(case_filter) {
   // HIDE ALL THE PROJECTS EXCEPT THE ONES OF THE SAME TYPE AS THE FILTER
   // IF ALL IS CLICKED, ALL WILL BE SHOWN
   type_filter = "type_".concat(case_filter.toLowerCase());
-  var allcases = ["type_web", "type_ml", "type_data", "type_other"];
+  var allcases = ["type_web", "type_ml", "type_data", "type_db", "type_other"];
 
   if (case_filter != "all"){
     for (var i = 0, ii = allcases.length; i < ii; i++) {
@@ -42,8 +42,10 @@ function selectButton(case_button) {
   document.getElementById("flt_btn_all").classList.add("btn-outline-primary");
   document.getElementById("flt_btn_web").classList.remove("btn-success");
   document.getElementById("flt_btn_web").classList.add("btn-outline-success");
-  document.getElementById("flt_btn_ml").classList.remove("btn-secondary");
-  document.getElementById("flt_btn_ml").classList.add("btn-outline-secondary");
+  document.getElementById("flt_btn_ml").classList.remove("btn-danger");
+  document.getElementById("flt_btn_ml").classList.add("btn-outline-danger");
+  document.getElementById("flt_btn_db").classList.remove("btn-secondary");
+  document.getElementById("flt_btn_db").classList.add("btn-outline-secondary");
   document.getElementById("flt_btn_data").classList.remove("btn-warning");
   document.getElementById("flt_btn_data").classList.add("btn-outline-warning");
   document.getElementById("flt_btn_other").classList.remove("btn-info");
@@ -56,8 +58,11 @@ function selectButton(case_button) {
     document.getElementById("flt_btn_web").classList.remove("btn-outline-success");
     document.getElementById("flt_btn_web").classList.add("btn-success");
   }else if (button_filter=="flt_btn_ml") {
-    document.getElementById("flt_btn_ml").classList.remove("btn-outline-secondary");
-    document.getElementById("flt_btn_ml").classList.add("btn-secondary");
+    document.getElementById("flt_btn_ml").classList.remove("btn-outline-danger");
+    document.getElementById("flt_btn_ml").classList.add("btn-danger");
+  }else if (button_filter=="flt_btn_db") {
+    document.getElementById("flt_btn_db").classList.remove("btn-outline-secondary");
+    document.getElementById("flt_btn_db").classList.add("btn-secondary");
   }else if (button_filter=="flt_btn_data") {
     document.getElementById("flt_btn_data").classList.remove("btn-outline-warning");
     document.getElementById("flt_btn_data").classList.add("btn-warning");
