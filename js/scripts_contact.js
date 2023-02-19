fbb2ae186dc75ceb258a25c647bdc32bb77203ee
lluisg <lluis.guardia.f@gmail.com>
2023-02-19 15:33:19 +0100

    try to fix contact form

--- js/scripts_contact.js
+++ js/scripts_contact.js
@@ -1,21 +1,9 @@
-function SendContactFrom(){
-  $.ajax({
-    method: 'POST',
-    url: 'https://formsubmit.co/ajax/f1aa8b04c973b5f015f60fb3e5245d09',
-    dataType: 'json',
-    accepts: 'application/json',
-    data: {
-      name: document.getElementById('contact-name'),
-      email: document.getElementById('contact-email'),
-      message: document.getElementById('contact-message'),
-      attachment: document.getElementById('contact-file'),
-      // next: '#about',
-      captcha: 'false',
-      autoresponse: 'Thank you for reaching out to me through the contact form on my portfolio website. I appreciate your interest and I will get back to you as soon as possible with a response.\nBest regards, Lluis.',
-      template: 'table',
-    },
-    success: (data) => console.log(data),
-    error: (err) => console.log(err)
-  });
-  console.log('hey')
+
+function ContactSended(){
+  const btn = document.getElementById('btn-contact-submit');
+  
+  btn.innerHTML = 'Message sent.\nTo send another refresh the page.'
+  btn.classList.remove("btn-dark");
+  btn.classList.add("btn-success");
+  btn.setAttribute('disabled', true);
 }
